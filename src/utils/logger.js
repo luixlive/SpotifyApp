863bf1caa3eee539947b5d75caae92f1d49693ed
Luis Alfonso <luis.chavez@wizeline.com>
2018-02-11 19:06:55 -0600

    Log transports depend on environment

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,24 +3,32 @@ const winston = require('winston');
 
 const logLevel = config.get('LOG_LEVEL');
 
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      colorize: true,
-      exitOnError: false,
-      level: logLevel,
-      prettyPrint: true,
-      timestamp: true,
-    }),
-    new (winston.transports.File)({
-      exitOnError: false,
-      filename: config.get('LOG_FILE'),
-      json: false,
-      level: logLevel,
-      timestamp: true,
-    }),
-  ],
+const consoleTransport = new (winston.transports.Console)({
+  colorize: true,
+  exitOnError: false,
+  level: logLevel,
+  prettyPrint: true,
+  timestamp: true,
 });
+const environment = config.get('NODE_ENV');
+const fileTransport = new (winston.transports.File)({
+  exitOnError: false,
+  filename: config.get('LOG_FILE'),
+  json: false,
+  level: logLevel,
+  timestamp: true,
+});
+let transports = [];
+
+if (environment === 'development') {
+  transports = transports.concat([consoleTransport, fileTransport]);
+} else if (environment === 'test') {
+  transports.push(fileTransport);
+} else {
+  transports.push(consoleTransport);
+}
+
+const logger = new (winston.Logger)({ transports });
 
 logger.info('Logger started');
 
