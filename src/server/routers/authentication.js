8d6cf5ce64e93e6fcb18c7dfa8cce9955a076d6b
Luis Alfonso <luis.chavez@wizeline.com>
2018-02-18 10:36:25 -0600

    Stringify objects on logs

--- src/server/routers/authentication.js
+++ src/server/routers/authentication.js
@@ -10,18 +10,18 @@ router.get('/spotify', passport.authenticate('spotify'));
 router.get('/spotify/callback', passport.authenticate('spotify', {
   failureRedirect: '/login',
 }), (req, res) => {
-  logger.debug(`api/authentication/spotify/callback: ${req.user}`);
+  logger.debug(`api/authentication/spotify/callback: ${JSON.stringify(req.user)}`);
   res.redirect('/stats');
 });
 
 router.get('/logout', (req, res) => {
-  logger.debug(`api/authentication/logout: ${req.user}`);
+  logger.debug(`api/authentication/logout: ${JSON.stringify(req.user)}`);
   req.logout();
   res.redirect('/');
 });
 
 router.get('/user', (req, res) => {
-  logger.debug(`api/authentication/user: ${req.user}`);
+  logger.debug(`api/authentication/user: ${JSON.stringify(req.user)}`);
   res.send(req.user);
 });
 
