1253970f629be387b9ac35d55a08415f931c10e0
Luis Alfonso <luis.chavez@wizeline.com>
2018-03-18 13:34:37 -0600

    Update test

--- src/test/app/api/authentication.test.js
+++ src/test/app/api/authentication.test.js
@@ -13,7 +13,7 @@ describe('App API - Authentication', () => {
       expect(fetch).toHaveBeenCalledTimes(1);
       expect(fetch.mock.calls[0]).toEqual([
         '/api/authentication/user',
-        { method: 'GET' },
+        { credentials: 'same-origin', method: 'GET' },
       ]);
     });
   });
