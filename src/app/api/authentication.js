0424a5aac68e7b3dd7ddcd7d08b7aa8806205e0e
Luis Alfonso <luis.chavez@wizeline.com>
2018-03-18 13:33:44 -0600

    Add same-origin credentials flag in fetch user

--- src/app/api/authentication.js
+++ src/app/api/authentication.js
@@ -1,6 +1,9 @@
 export default {
   user: {
-    get: () => window.fetch('/api/authentication/user', { method: 'GET' }),
+    get: () => window.fetch('/api/authentication/user', {
+      credentials: 'same-origin',
+      method: 'GET',
+    }),
   },
   logout: {
     get: () => window.fetch('/api/authentication/logout', { method: 'GET' }),
