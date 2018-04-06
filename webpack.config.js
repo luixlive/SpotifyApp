7026d538b3a30d7d0fb0895e190f91b36d7da864
Luis Alfonso <luixlive@gmail.com>
2018-04-06 09:05:57 -0500

    Minor configuratoin changes

--- webpack.config.js
+++ webpack.config.js
@@ -6,72 +6,72 @@ module.exports = {
   entry: [
     'babel-polyfill',
     ...(process.env.NODE_ENV !== 'production' ? [
-      'webpack-hot-middleware/client'
+      'webpack-hot-middleware/client',
     ] : []),
     'whatwg-fetch',
-    path.resolve(__dirname, './src/app/index.jsx')
+    path.resolve(__dirname, './src/app/index.jsx'),
   ],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   module: {
     rules: [{
       test: /\.(js|jsx)$/,
       use: 'babel-loader',
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }, {
       test: /\.css$/,
       use: [{
-        loader: 'style-loader'
+        loader: 'style-loader',
       }, {
         loader: 'css-loader',
         options: {
-          modules: true
-        }
-      }]
+          modules: true,
+        },
+      }],
     }, {
       test: /\.(eot|png|ttf|svg|woff|woff2)$/,
-      use: 'file-loader'
+      use: 'file-loader',
     }, {
       test: /\.less$/,
       use: [{
-          loader: "style-loader"
+        loader: 'style-loader',
       }, {
-          loader: "css-loader"
+        loader: 'css-loader',
       }, {
-          loader: "less-loader"
-      }]
-    }]
+        loader: 'less-loader',
+      }],
+    }],
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: 'body',
       filename: 'index.html',
-      template: './src/app/index.html'
+      template: './src/app/index.html',
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     ...(process.env.NODE_ENV !== 'production' ? [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
     ] : []),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false
+      debug: false,
     }),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
   ],
   resolve: {
     // Hack to solve Semantic UI issue reading theme.config
     alias: {
-      '../../theme.config$': path.join(__dirname, 'src/app/style/theme.config')
+      '../../theme.config$': path.join(__dirname, 'src/app/style/theme.config'),
     },
-    extensions: [".js", ".jsx"]
-  }
+    extensions: ['.js', '.jsx'],
+  },
 };
