cf4244a0f680d471f11c7817bc9a7b2ba86b2c23
Luis Alfonso <luixlive@gmail.com>
2018-04-24 23:08:03 -0500

    Create artist card based on track card

--- src/app/components/containers/user_stats/artists/artist_list.jsx
+++ src/app/components/containers/user_stats/artists/artist_list.jsx
@@ -1,24 +1,30 @@
 import { connect } from 'react-redux';
+import { Grid, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import { ScreenLoader } from './../../../templates';
+import ArtistCard from './artist_card';
 
 export const PureArtistList = (props) => {
   if (props.reloading) {
     return <ScreenLoader />;
   }
   return (
-    <ol>
-      {props.artists.map(artist => (
-        <li key={artist.id}>{artist.name}</li>
-      ))}
-    </ol>
+    <Segment basic>
+      <Grid>
+        {props.artists.map((artist, index) => (
+          <Grid.Column key={artist.id} width={props.deviceMobile ? 16 : 8}>
+            <ArtistCard {...artist} place={index + 1} />
+          </Grid.Column>
+        ))}
+      </Grid>
+    </Segment>
   );
 };
 
 PureArtistList.propTypes = {
-  reloading: PropTypes.bool.isRequired,
+  deviceMobile: PropTypes.bool.isRequired,
   artists: PropTypes.arrayOf(PropTypes.shape({
     followers: PropTypes.number,
     genres: PropTypes.array,
@@ -28,9 +34,11 @@ PureArtistList.propTypes = {
     popularity: PropTypes.number,
     spotifyUrl: PropTypes.string,
   })).isRequired,
+  reloading: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = ({ userStats }) => ({
+const mapStateToProps = ({ deviceMobile, userStats }) => ({
+  deviceMobile,
   reloading: userStats.topArtists.reloading,
   artists: userStats.topArtists.list,
 });
