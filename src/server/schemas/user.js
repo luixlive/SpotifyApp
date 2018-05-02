a006d4686211a9e52a19f6eabb1cf9325f4d105c
Luis Alfonso <luixlive@gmail.com>
2018-05-01 19:27:33 -0500

    Looks like display name is not mandatory

--- src/server/schemas/user.js
+++ src/server/schemas/user.js
@@ -4,7 +4,7 @@ module.exports = Joi.object().keys({
   accessToken: Joi.string().min(1).required(),
   expires: Joi.number().integer().positive().required(),
   profile: Joi.object().keys({
-    displayName: Joi.string().required(),
+    displayName: Joi.string(),
     spotifyUrl: Joi.string().uri().required(),
     followers: Joi.number().integer().min(0).required(),
     id: Joi.string().required(),
