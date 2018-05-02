8fc4a4ee7a87b9817946c2165b774e5564274e15
Luis Alfonso <luixlive@gmail.com>
2018-05-01 19:39:36 -0500

    Allow null displayName

--- src/server/schemas/user.js
+++ src/server/schemas/user.js
@@ -4,7 +4,7 @@ module.exports = Joi.object().keys({
   accessToken: Joi.string().min(1).required(),
   expires: Joi.number().integer().positive().required(),
   profile: Joi.object().keys({
-    displayName: Joi.string(),
+    displayName: Joi.string().allow(null),
     spotifyUrl: Joi.string().uri().required(),
     followers: Joi.number().integer().min(0).required(),
     id: Joi.string().required(),
